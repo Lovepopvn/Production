dd40a00ab3c75687af21bd725d9e780e2f007af4
Lukáš Halda <lukas@portcities.vn>
2020-11-01 12:02:45 +0700

    [ADD] Synchronized with GitLab development 20/11/01 11:50 - commit a1d77b38392c6a2b12f96e8c1716a9e10ad60f9a

--- lp_mrp/models/mrp_production.py
+++ lp_mrp/models/mrp_production.py
@@ -27,6 +27,7 @@ class MrpProduction(models.Model):
     parent_mo_id = fields.Many2one('mrp.production','Parent MO')
     mo_for_samples = fields.Boolean('MO for Samples')
     expected_ship_date = fields.Datetime('Expected Ship Date')
+    picking_date = fields.Date()
 
     @api.depends('move_raw_ids.state', 'move_finished_ids.state', 'workorder_ids', 'workorder_ids.state', 'qty_produced', 'move_raw_ids.quantity_done', 'product_qty')
     def _compute_state(self):
